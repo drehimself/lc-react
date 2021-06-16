67bf4d1b48b95991e2fd67158c72f31f3e0ef104
Andre Madarang <andre_madarang@hotmail.com>
2021-06-16 15:11:55 -0400

    Episode 1 - Intro

---
+++ src/Another.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Another(props) {
+  return (
+    <div>
+      Another Component, { props.name }
+    </div>
+  )
+}
---
+++ src/App.jsx
@@ -0,0 +1,53 @@
+import { useState } from 'react';
+import Another from './Another';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  function decrement() {
+    setCount(prevCount => prevCount - 1);
+  }
+
+  function increment() {
+    setCount(prevCount => prevCount + 1);
+  }
+
+  const someStyle = {
+    background: 'blue',
+    color: 'white',
+    fontSize: '28px',
+    fontWeight: 'bold',
+  }
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Another name="Andre" />
+        <div>
+          <span>{count}</span>
+          <button onClick={decrement}>-</button>
+          <button onClick={increment}>+</button>
+        </div>
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        { true &&
+          <p style={someStyle}>{ 3 + 2}</p>
+        }
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
